bfd72d6870f14ed5e2c86bfe89dde7587762e4a6
Andrey Sak <andrei.sak@banuba.com>
2024-01-22 18:15:24 +0300

    refactoring ios plugin implementation

--- ios/Classes/ArEffectWrapper.swift
+++ ios/Classes/ArEffectWrapper.swift
@@ -1,35 +1,67 @@
-import Foundation
+import BanubaARCloudSDK
 
-internal struct ArEffectWrapper: Hashable, Encodable {
-    public var isDefault: Bool
-    public var eTag: String?
-    public var id: Int
-    public var name: String
-    public var preview: String
-    public var type: String?
-    public var uri: String
-    public var isDownloaded: Bool
-    
-    enum CodingKeys: String, CodingKey {
-        case `default`
-        case eTag
-        case id
-        case name
-        case preview
-        case type
-        case uri
-        case isDownloaded
+struct ArEffectWrapper: Hashable, Encodable {
+  var isDefault: Bool
+  var eTag: String?
+  var id: Int
+  var name: String
+  var preview: String
+  var type: String?
+  var uri: String
+  var isDownloaded: Bool
+  
+  enum CodingKeys: String, CodingKey {
+    case `default`
+    case eTag
+    case id
+    case name
+    case preview
+    case type
+    case uri
+    case isDownloaded
+  }
+  
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(isDefault, forKey: .`default`)
+    try container.encode(eTag, forKey: .eTag)
+    try container.encode(id, forKey: .id)
+    try container.encode(name, forKey: .name)
+    try container.encode(preview, forKey: .preview)
+    try container.encode(type, forKey: .type)
+    try container.encode(uri, forKey: .uri)
+    try container.encode(isDownloaded, forKey: .isDownloaded)
+  }
+}
+
+struct ArEffectsJsonWrapper: Hashable, Encodable {
+  var errorCode: String?
+  var effects: [ArEffectWrapper]?
+}
+
+extension Array where Element == AREffect {
+  func wrapped() -> [ArEffectWrapper] {
+    self.map { effect in
+      ArEffectWrapper(
+        isDefault: false,
+        eTag: nil,
+        id: effect.title.hash,
+        name: effect.title,
+        preview: effect.isDownloaded ? "\(effect.localURL?.path ?? "")/preview.png" : effect.previewImage.absoluteString,
+        type: effect.type,
+        uri: effect.isDownloaded ? effect.localURL?.path ?? "" : effect.downloadLink.path,
+        isDownloaded: effect.isDownloaded
+      )
     }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(isDefault, forKey: .`default`)
-        try container.encode(eTag, forKey: .eTag)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(preview, forKey: .preview)
-        try container.encode(type, forKey: .type)
-        try container.encode(uri, forKey: .uri)
-        try container.encode(isDownloaded, forKey: .isDownloaded)
-    }
+  }
+}
+
+extension Encodable {
+  func asPrettyPrintedString() -> String {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    let jsonData = try! encoder.encode(self)
+    let jsonString = String(data: jsonData, encoding: .utf8)!
+    return jsonString
+  }
 }
--- ios/banuba_arcloud.podspec
+++ ios/banuba_arcloud.podspec
@@ -5,9 +5,9 @@ Pod::Spec.new do |s|
     s.description      = <<-DESC
   A new flutter plugin project.
                          DESC
-    s.homepage         = 'http://example.com'
+    s.homepage         = 'https://banuba.com'
     s.license          = { :file => '../LICENSE' }
-    s.author           = { 'Your Company' => 'email@example.com' }
+    s.author           = { 'Your Company' => 'info@banuba.com' }
     s.source           = { :path => '.' }
     s.source_files = 'Classes/**/*'
     s.dependency 'Flutter'
